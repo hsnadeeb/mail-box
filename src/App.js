dc357a28a96f655b23a0e0b01675b032440a80fb
hsnadeeb <hsnadeeb007@gmail.com>
2023-12-11 11:55:01 +0530

    wrote auth test cases

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import AuthForm from './components/auth/AuthForm';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Routes>
     <div className="App">
       <AuthForm />
     </div>
+    </Routes>
   );
 }
+
+{/* <Routes>
+        <Route path='/' element={<HomePage />} />
+        {!authCtx.isLoggedIn && <Route path='/auth' element={<AuthPage />} />}
+        {authCtx.isLoggedIn && <Route path='/profile' element={<UserProfile />} />}
+      </Routes> */}
 
 export default App;
